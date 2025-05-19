ad52cee9fa78bf716ed5064198c14d90ab3ff5d0
Bruno Garcia <itsme@bgarciamoura.com>
2025-05-19 20:44:53 -0300

    feat: Usando o expo-secure-store de outro app

--- apps/security/src/app/App.tsx
+++ apps/security/src/app/App.tsx
@@ -1,11 +1,36 @@
 import { Text, View } from "react-native";
+import * as SecureStore from "expo-secure-store";
 
 const App = () => {
-  return (
-    <View>
-      <Text>Welcome to the Security App!</Text>
-    </View>
-  );
+	const storeData = async (key: string, value: string) => {
+		try {
+			await SecureStore.setItemAsync(key, value);
+			console.log("Data stored successfully");
+		} catch (error) {
+			console.error("Error storing data", error);
+		}
+	};
+
+	const getData = async (key: string) => {
+		try {
+			const value = await SecureStore.getItemAsync(key);
+			if (value !== null) {
+				console.log("Retrieved data:", value);
+			} else {
+				console.log("No data found for key:", key);
+			}
+		} catch (error) {
+			console.error("Error retrieving data", error);
+		}
+	};
+
+	return (
+		<View>
+			<Text>Welcome to the Security App!</Text>
+			<Text onPress={() => storeData("testKey", "testValue")}>Store Data</Text>
+			<Text onPress={() => getData("testKey")}>Get Data</Text>
+		</View>
+	);
 };
 
 export { App };
